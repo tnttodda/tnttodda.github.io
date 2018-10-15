c30fd7f3e0096aeb1034a22ef4d4fa5cb39c1e5f
Todd Waugh Ambridge <txw467@bham.ac.uk>
2018-10-15 17:13:13 +0100

    changed example to something relevant

--- CBV-with-CBV-embedding/js/examples.js
+++ CBV-with-CBV-embedding/js/examples.js
@@ -1,8 +1,10 @@
-var fact_prog = 
-  'let fact = rec f. λx.\n'
-+ '  if (x <= 1)\n'
-+ '  then 1\n'
-+ '  else (x * (f (x - 1)))\n'
-+ 'in\n'
-+ '\n'
-+ 'fact 4';
+// var fact_prog =
+//   'let fact = rec f. λx.\n'
+// + '  if (x <= 1)\n'
+// + '  then 1\n'
+// + '  else (x * (f (x - 1)))\n'
+// + 'in\n'
+// + '\n'
+// + 'fact 4';
+
+var fact_prog = 'bind x = 2 in PLUS(1,x)'
